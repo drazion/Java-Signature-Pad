c042753d6b4fb23b87ccb885200f4e28a280f8d8
drazion <aaron.harvey@gmail.com>
2013-02-27 08:09:34 -0500

    Initial Commit

---
+++ post-image.php
@@ -0,0 +1,40 @@
+<?php
+$imageName = $_FILES['uploadedfile']['name'];
+
+/**********************************************/
+/* MODIFY THESE VALUES TO REFLECT YOUR SERVER */
+/* CONFIGURATION                              */
+/**********************************************/
+//Tracks user signature submission by IP
+$session_id = $_SERVER['REMOTE_ADDR'];
+
+//mySQL login information
+$mySQLuser = 'admin';
+$mySQLpass = 'password';
+$mySQLserver = 'localhost';
+$mySQLdatabase = 'database';
+$mySQLtable = 'table';
+$link = mysql_connect($mySQLserver,$mySQLuser, $mySQLpass) OR DIE(mysql_error);
+mysql_select_db($mySQLdatabase, $link);
+
+//This is the directory where the signature will be uploaded
+$target_path = "uploads/";
+
+
+
+/***************************/
+/* DO NOT MODIFY PAST HERE */
+/***************************/
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+$link = mysql_connect('localhost','thepower_bue', 'dkj32p3m') OR DIE(mysql_error);
+    mysql_select_db("$mySQLdatabase", $link);
+    mysql_query("INSERT INTO `$mySQLtabe` (`session`, `image_location`) VALUES ('$session_id', '$imageName')") OR DIE(mysql_error());
+    mysql_close($link);
+    
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) { 
+    echo "The file ". basename( $_FILES['uploadedfile']['name'])." has been uploaded";            
+} else{ 
+    echo "There was an error uploading the file, please try again!"; 
+}
+
+?>
