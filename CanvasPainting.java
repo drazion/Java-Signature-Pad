842be3a7fa72b08b9688d72447ba5b793c93e282
Aaron <aaron.harvey@gmail.com>
2013-02-27 08:18:31 -0500

    Update CanvasPainting.java

--- CanvasPainting.java
+++ CanvasPainting.java
@@ -5,7 +5,7 @@ package signature;
  * Build Date: April 20, 2012
  * Copyright 2012 Aaron Harvey
 
-   Copyright (c) 2013 Aaron Harvey
+   Copyright (c) 2012 Aaron Harvey
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
